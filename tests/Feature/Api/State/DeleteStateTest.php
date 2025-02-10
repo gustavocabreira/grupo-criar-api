687c719fe408e59b4b712e39d52885fce276ba72
Gustavo Cabreira <gustavo.softdev@gmail.com>
2025-02-10 14:18:35 -0300

    test: it should return not found when trying to delete a state that does not exists

--- tests/Feature/Api/State/DeleteStateTest.php
+++ tests/Feature/Api/State/DeleteStateTest.php
@@ -13,3 +13,17 @@ test('it should be able to delete a state', function() {
 
     $this->assertDatabaseMissing($model->getTable(), $state->toArray());
 });
+
+test('it should return not found when trying to delete a state that does not exists', function() {
+    $model = new State;
+
+    $state = State::factory()->create();
+
+    $response = $this->deleteJson(route("api.states.destroy", ['state' => -1]));
+
+    $response
+        ->assertStatus(Response::HTTP_NOT_FOUND)
+        ->assertJsonPath('message', 'No query results for model [App\Models\State] -1');
+
+    $this->assertDatabaseHas($model->getTable(), $state->toArray());
+});
