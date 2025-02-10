d69fd22feacef73291391adf3db4bc26ce8eb034
Gustavo Cabreira <gustavo.softdev@gmail.com>
2025-02-10 15:42:59 -0300

    test: it should return not found when trying to update a city that does not exist

--- tests/Feature/Api/City/UpdateCityTest.php
+++ tests/Feature/Api/City/UpdateCityTest.php
@@ -14,3 +14,13 @@ test('it should be able to update a city', function () {
 
     $this->assertDatabaseHas($model->getTable(), ['id' => $city->id, ...$payload]);
 });
+
+test('it should return not found when trying to update a city that does not exist', function () {
+    $payload = City::factory()->make()->toArray();
+
+    $response = $this->putJson(route('api.cities.update', ['city' => -1]), $payload);
+
+    $response
+        ->assertStatus(Response::HTTP_NOT_FOUND)
+        ->assertJsonPath('message', 'No query results for model [App\Models\City] -1');
+});
