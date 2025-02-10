58c54c6b40de508ba7d6106c1d844ac423e05732
Gustavo Cabreira <gustavo.softdev@gmail.com>
2025-02-10 15:49:32 -0300

    test: it should return not found when trying to delete a city that does not exists

--- tests/Feature/Api/City/DeleteCityTest.php
+++ tests/Feature/Api/City/DeleteCityTest.php
@@ -13,3 +13,17 @@ test('it should be able to delete a city', function () {
 
     $this->assertDatabaseMissing($model->getTable(), $city->toArray());
 });
+
+test('it should return not found when trying to delete a city that does not exists', function () {
+    $model = new City;
+
+    $city = City::factory()->create();
+
+    $response = $this->deleteJson(route('api.cities.destroy', ['city' => -1]));
+
+    $response
+        ->assertStatus(Response::HTTP_NOT_FOUND)
+        ->assertJsonPath('message', 'No query results for model [App\Models\City] -1');
+
+    $this->assertDatabaseHas($model->getTable(), $city->toArray());
+});
