4e7fd38f218b87a23bf653bb9c9f8627f972b514
Gustavo Cabreira <gustavo.softdev@gmail.com>
2025-02-10 14:06:09 -0300

    test: it should return not found when trying to update a state that does not exist

--- tests/Feature/Api/State/UpdateStateTest.php
+++ tests/Feature/Api/State/UpdateStateTest.php
@@ -15,3 +15,13 @@ test('it should be able to update a state', function () {
 
     $this->assertDatabaseHas($model->getTable(), ['id' => $state->id, ...$payload]);
 });
+
+test('it should return not found when trying to update a state that does not exist', function () {
+    $payload = State::factory()->make()->toArray();
+
+    $response = $this->putJson(route('api.states.update', ['state' => -1]), $payload);
+
+    $response
+        ->assertStatus(Response::HTTP_NOT_FOUND)
+        ->assertJsonPath('message', 'No query results for model [App\Models\State] -1');
+});
