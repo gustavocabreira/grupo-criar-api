3a938a8e71dfdfab287df2def72c25a9d8dbf606
Gustavo Cabreira <gustavo.softdev@gmail.com>
2025-02-10 14:26:15 -0300

    refactor: runs pint

--- tests/Feature/Api/State/DeleteStateTest.php
+++ tests/Feature/Api/State/DeleteStateTest.php
@@ -3,23 +3,23 @@
 use App\Models\State;
 use Illuminate\Http\Response;
 
-test('it should be able to delete a state', function() {
+test('it should be able to delete a state', function () {
     $model = new State;
     $state = State::factory()->create();
 
-    $response = $this->deleteJson(route("api.states.destroy", ['state' => $state->id]));
+    $response = $this->deleteJson(route('api.states.destroy', ['state' => $state->id]));
 
     $response->assertStatus(Response::HTTP_NO_CONTENT);
 
     $this->assertDatabaseMissing($model->getTable(), $state->toArray());
 });
 
-test('it should return not found when trying to delete a state that does not exists', function() {
+test('it should return not found when trying to delete a state that does not exists', function () {
     $model = new State;
 
     $state = State::factory()->create();
 
-    $response = $this->deleteJson(route("api.states.destroy", ['state' => -1]));
+    $response = $this->deleteJson(route('api.states.destroy', ['state' => -1]));
 
     $response
         ->assertStatus(Response::HTTP_NOT_FOUND)
