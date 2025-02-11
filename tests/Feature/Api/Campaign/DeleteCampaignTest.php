4f772cf75d3dd5a9c30d99ece7485a3814875531
Gustavo Cabreira <gustavo.softdev@gmail.com>
2025-02-11 16:50:10 -0300

    test: it should return not found when trying to delete a city that does not exists

--- tests/Feature/Api/Campaign/DeleteCampaignTest.php
+++ tests/Feature/Api/Campaign/DeleteCampaignTest.php
@@ -13,3 +13,17 @@ test('it should be able to delete a campaign', function () {
 
     $this->assertDatabaseMissing($model->getTable(), $campaign->toArray());
 });
+
+test('it should return not found when trying to delete a city that does not exists', function () {
+    $model = new Campaign();
+
+    $campaign = Campaign::factory()->create();
+
+    $response = $this->deleteJson(route('api.campaigns.destroy', ['campaign' => -1]));
+
+    $response
+        ->assertStatus(Response::HTTP_NOT_FOUND)
+        ->assertJsonPath('message', 'No query results for model [App\Models\Campaign] -1');
+
+    $this->assertDatabaseHas($model->getTable(), $campaign->toArray());
+});
