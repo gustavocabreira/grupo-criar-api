9b82a85a8d17bce8fdcede6dd8ea78b63d6516a4
Gustavo Cabreira <gustavo.softdev@gmail.com>
2025-02-11 18:37:44 -0300

    test: it should return not found when trying to delete a discount that does not exists

--- tests/Feature/Api/Discount/DeleteDiscountTest.php
+++ tests/Feature/Api/Discount/DeleteDiscountTest.php
@@ -12,3 +12,17 @@ test('it should be able to delete a discount', function () {
 
     $this->assertDatabaseMissing($model->getTable(), $discount->toArray());
 });
+
+test('it should return not found when trying to delete a discount that does not exists', function () {
+    $model = new Discount();
+
+    $discount = Discount::factory()->create();
+
+    $response = $this->deleteJson(route('api.discounts.destroy', ['discount' => -1]));
+
+    $response
+        ->assertStatus(404)
+        ->assertJsonPath('message', 'No query results for model [App\Models\Discount] -1');
+
+    $this->assertDatabaseHas($model->getTable(), $discount->toArray());
+});
