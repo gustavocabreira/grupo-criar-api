54f9ebed2bb2d28339a753cf5e60b374278672ca
Gustavo Cabreira <gustavo.softdev@gmail.com>
2025-02-11 16:46:11 -0300

    test: it should return not found when trying to update a campaign that does not exist

--- tests/Feature/Api/Campaign/UpdateCampaignTest.php
+++ tests/Feature/Api/Campaign/UpdateCampaignTest.php
@@ -14,3 +14,13 @@ test('it should be able to update a campaign', function () {
 
     $this->assertDatabaseHas($model->getTable(), ['id' => $campaign->id, ...$payload]);
 });
+
+test('it should return not found when trying to update a campaign that does not exist', function () {
+    $payload = Campaign::factory()->make()->toArray();
+
+    $response = $this->putJson(route('api.campaigns.update', ['campaign' => -1]), $payload);
+
+    $response
+        ->assertStatus(Response::HTTP_NOT_FOUND)
+        ->assertJsonPath('message', 'No query results for model [App\Models\Campaign] -1');
+});
