880a96c4b6d76e14f28582eef512ad7160c72677
Gustavo Cabreira <gustavo.softdev@gmail.com>
2025-02-11 18:27:28 -0300

    test: it should return not found when trying to update a discount that does not exist

--- tests/Feature/Api/Discount/UpdateDiscountTest.php
+++ tests/Feature/Api/Discount/UpdateDiscountTest.php
@@ -14,3 +14,13 @@ test('it should be able to update a discount', function () {
 
     $this->assertDatabaseHas($model->getTable(), ['id' => $discount->id, ...$payload]);
 });
+
+test('it should return not found when trying to update a discount that does not exist', function () {
+    $payload = Discount::factory()->make()->toArray();
+
+    $response = $this->putJson(route('api.discounts.update', ['discount' => -1]), $payload);
+
+    $response
+        ->assertStatus(Response::HTTP_NOT_FOUND)
+        ->assertJsonPath('message', 'No query results for model [App\Models\Discount] -1');
+});
