d1e8bf66da62d2fef386d639cdc81b74eaeb15d1
Gustavo Cabreira <gustavo.softdev@gmail.com>
2025-02-11 17:27:59 -0300

    test: it should be able to remove multiple clusters from a campaign

--- tests/Feature/Api/Campaign/Cluster/RemoveClustersFromCampaignTest.php
+++ tests/Feature/Api/Campaign/Cluster/RemoveClustersFromCampaignTest.php
@@ -32,3 +32,31 @@ test('it should be able to remove a cluster from a campaign', function () {
         ]);
     }
 });
+
+test('it should be able to remove multiple clusters from a campaign', function () {
+    $campaign = Campaign::factory()->create();
+    $clusters = Cluster::factory()->count(3)->create();
+
+    $campaign->clusters()->attach($clusters->pluck('id')->toArray(), ['is_active' => true]);
+
+    $payload = [
+        'clusters' => [$clusters->first()->id, $clusters->last()->id],
+    ];
+
+    $response = $this->deleteJson(route('api.campaigns.clusters.destroy', ['campaign' => $campaign->id]), $payload);
+    $response->assertStatus(Response::HTTP_NO_CONTENT);
+
+    foreach ($payload as $cluster) {
+        $this->assertDatabaseHas('cluster_campaign_pivot', [
+            'campaign_id' => $campaign->id,
+            'cluster_id' => $cluster,
+            'is_active' => false,
+        ]);
+    }
+
+    $this->assertDatabaseHas('cluster_campaign_pivot', [
+        'campaign_id' => $campaign->id,
+        'cluster_id' => $clusters[1]->id,
+        'is_active' => true,
+    ]);
+});
