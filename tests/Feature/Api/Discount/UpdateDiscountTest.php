b1614ffab71b5604973eca67a377c17da9f82ccd
Gustavo Cabreira <gustavo.softdev@gmail.com>
2025-02-11 18:31:42 -0300

    test: it should return unprocessable entity when trying to update a discount with an invalid payload

--- tests/Feature/Api/Discount/UpdateDiscountTest.php
+++ tests/Feature/Api/Discount/UpdateDiscountTest.php
@@ -2,6 +2,7 @@
 
 use App\Models\Discount;
 use Illuminate\Http\Response;
+use Illuminate\Support\Str;
 
 test('it should be able to update a discount', function () {
     $model = new Discount();
@@ -24,3 +25,49 @@ test('it should return not found when trying to update a discount that does not 
         ->assertStatus(Response::HTTP_NOT_FOUND)
         ->assertJsonPath('message', 'No query results for model [App\Models\Discount] -1');
 });
+
+
+dataset('invalid_payload', [
+    'empty name' => [
+        ['name' => ''], ['name' => ['The name field is required.']],
+    ],
+    'name with more than 255 characters' => [
+        ['name' => Str::repeat('*', 256)], ['name' => ['The name field must not be greater than 255 characters.']],
+    ],
+    'description with more than 255 characters' => [
+        ['description' => Str::repeat('*', 256)], ['description' => ['The description field must not be greater than 255 characters.']],
+    ],
+    'negative value' => [
+        ['value' => '-1'], ['value' => ['The value field must be at least 0.']],
+    ],
+    'negative percentage' => [
+        ['percentage' => '-1'], ['percentage' => ['The percentage field must be at least 0.']],
+    ],
+    'percentage greater than 100' => [
+        ['percentage' => '101'], ['percentage' => ['The percentage field must not be greater than 100.']],
+    ],
+    'both value and percentage missing' => [
+        [], ['value' => ['The value field is required when percentage is not present.'], 'percentage' => ['The percentage field is required when value is not present.']],
+    ],
+]);
+
+test('it should return unprocessable entity when trying to update a discount with an invalid payload', function ($payload, $expectedErrors) {
+    $model = new Discount();
+
+    $discount = Discount::factory()->create();
+
+    $response = $this->putJson(route('api.discounts.update', ['discount' => $discount->id]), $payload);
+
+    $response->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY);
+    $response->assertJsonValidationErrors(array_keys($expectedErrors));
+
+    foreach ($expectedErrors as $key => $errorMessages) {
+        $response->assertJsonPath("errors.$key", $errorMessages);
+    }
+
+    if (count($payload) > 0) {
+        $this->assertDatabaseMissing($model->getTable(), $payload);
+    }
+
+    $this->assertDatabaseCount($model->getTable(), 1);
+})->with('invalid_payload');
