7e59d5a89ed8ac94f79607ca4629a50cb7e69356
Gustavo Cabreira <gustavo.softdev@gmail.com>
2025-02-11 15:49:16 -0300

    test: it should return unprocessable entity when trying to remove a city from a cluster with an invalid payload

--- tests/Feature/Api/Cluster/City/RemoveCitiesFromClusterTest.php
+++ tests/Feature/Api/Cluster/City/RemoveCitiesFromClusterTest.php
@@ -63,3 +63,40 @@ test('it should be able to remove multiple cities from a cluster', function () {
         'is_active' => true,
     ]);
 });
+
+dataset('invalid_payload', [
+    'empty cities' => [
+        ['cities' => []], ['cities' => 'The cities field is required.'],
+    ],
+    'city that does not exist' => [
+        ['cities' => [-1]], ['cities.0' => 'The selected cities.0 is invalid.'],
+    ],
+]);
+
+test('it should return unprocessable entity when trying to remove a city from a cluster with an invalid payload', function ($payload, $expectedErrors) {
+    $key = array_keys($expectedErrors);
+    $city = City::factory()->create();
+
+    $cluster = Cluster::factory()->create();
+
+    $response = $this->postJson(route('api.clusters.cities.destroy', ['cluster' => $cluster->id]), $payload);
+
+    $response->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY);
+
+    $response->assertJsonValidationErrors($key);
+
+    $response->assertJsonFragment([
+        'errors' => [
+            $key[0] => [$expectedErrors[$key[0]]],
+        ],
+    ]);
+
+    if (! empty($payload['cities'])) {
+        $this->assertDatabaseMissing('cluster_city_pivot', [
+            'city_id' => $payload['cities'][0],
+        ]);
+    }
+
+    $this->assertDatabaseCount('cluster_city_pivot', 0);
+})->with('invalid_payload');
+
