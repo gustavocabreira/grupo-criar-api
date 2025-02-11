e138c39bc5558509d89ffeb4662b74d683c4ca3b
Gustavo Cabreira <gustavo.softdev@gmail.com>
2025-02-11 13:47:39 -0300

    test: it should return not found when trying to update a cluster that does not exist

--- tests/Feature/Api/Cluster/UpdateClusterTest.php
+++ tests/Feature/Api/Cluster/UpdateClusterTest.php
@@ -14,3 +14,13 @@ test('it should be able to update a cluster', function () {
 
     $this->assertDatabaseHas($model->getTable(), ['id' => $cluster->id, ...$payload]);
 });
+
+test('it should return not found when trying to update a cluster that does not exist', function () {
+    $payload = Cluster::factory()->make()->toArray();
+
+    $response = $this->putJson(route('api.clusters.update', ['cluster' => -1]), $payload);
+
+    $response
+        ->assertStatus(Response::HTTP_NOT_FOUND)
+        ->assertJsonPath('message', 'No query results for model [App\Models\Cluster] -1');
+});
