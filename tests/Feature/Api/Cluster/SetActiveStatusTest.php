1d2efd0daab9b9b8be4220cfab826da7600b53a1
Gustavo Cabreira <gustavo.softdev@gmail.com>
2025-02-11 13:58:38 -0300

    test: it should return not found when trying to set status of a cluster that does not exist

--- tests/Feature/Api/Cluster/SetActiveStatusTest.php
+++ tests/Feature/Api/Cluster/SetActiveStatusTest.php
@@ -20,3 +20,15 @@ test('it should be able to make a cluster active', function () {
         ...$payload,
     ]);
 });
+
+test('it should return not found when trying to set status of a cluster that does not exist', function () {
+    $payload = [
+        'is_active' => false,
+    ];
+
+    $response = $this->putJson(route('api.clusters.update', ['cluster' => -1]), $payload);
+
+    $response
+        ->assertStatus(Response::HTTP_NOT_FOUND)
+        ->assertJsonPath('message', 'No query results for model [App\Models\Cluster] -1');
+});
