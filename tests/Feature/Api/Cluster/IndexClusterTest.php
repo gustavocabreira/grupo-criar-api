a25572f471e5c893e6b9f1095b701723db1d33a0
Gustavo Cabreira <gustavo.softdev@gmail.com>
2025-02-11 14:01:40 -0300

    test: it should be able to set how many clusters will be returned per page

--- tests/Feature/Api/Cluster/IndexClusterTest.php
+++ tests/Feature/Api/Cluster/IndexClusterTest.php
@@ -34,3 +34,20 @@ test('it should be able to list all clusters paginated', function () {
         ->and($response->json()['current_page'])->toBe(1)
         ->and($response->json()['total'])->toBe(5);
 });
+
+test('it should be able to set how many clusters will be returned per page', function () {
+    Cluster::factory()->createMany(5);
+
+    $payload = [
+        'perPage' => 2,
+    ];
+
+    $response = $this->getJson(route('api.clusters.index', $payload));
+
+    $response->assertStatus(Response::HTTP_OK);
+
+    expect($response->json()['data'])->toHaveCount($payload['perPage'])
+        ->and($response->json()['current_page'])->toBe(1)
+        ->and($response->json()['per_page'])->toBe($payload['perPage'])
+        ->and($response->json()['total'])->toBe(5);
+});
