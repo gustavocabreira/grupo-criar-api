59d216051a681c021289835d02323aef918be8ea
Gustavo Cabreira <gustavo.softdev@gmail.com>
2025-02-12 13:31:53 -0300

    test: it should be able to assign multiple discounts to a campaign

--- tests/Feature/Api/Campaign/Discount/AssignDiscountToCampaignTest.php
+++ tests/Feature/Api/Campaign/Discount/AssignDiscountToCampaignTest.php
@@ -24,3 +24,25 @@ test('it should be able to assign a discount to a campaign', function () {
 
     $this->assertDatabaseCount('campaign_discount_pivot', 1);
 });
+
+test('it should be able to assign multiple discounts to a campaign', function () {
+    $campaign = Campaign::factory()->create();
+    $discount = Discount::factory()->count(2)->create();
+
+    $payload = [
+        'discounts' => $discount->pluck('id')->toArray(),
+    ];
+
+    $response = $this->postJson(route('api.campaigns.assign-discounts', ['campaign' => $campaign->id]), $payload);
+
+    $response->assertStatus(Response::HTTP_CREATED);
+
+    foreach ($discount as $discount) {
+        $this->assertDatabaseHas('campaign_discount_pivot', [
+            'campaign_id' => $campaign->id,
+            'discount_id' => $discount->id,
+        ]);
+    }
+
+    $this->assertDatabaseCount('campaign_discount_pivot', 2);
+});
