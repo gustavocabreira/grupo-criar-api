caf6819a35590b3f7b4ed65a211ae73ae1ac695f
Gustavo Cabreira <gustavo.softdev@gmail.com>
2025-02-12 13:32:51 -0300

    test: it should return unprocessable entity when trying to assign a new discount to a campaign with an invalid payload

--- tests/Feature/Api/Campaign/Discount/AssignDiscountToCampaignTest.php
+++ tests/Feature/Api/Campaign/Discount/AssignDiscountToCampaignTest.php
@@ -46,3 +46,39 @@ test('it should be able to assign multiple discounts to a campaign', function ()
 
     $this->assertDatabaseCount('campaign_discount_pivot', 2);
 });
+
+dataset('invalid_payload', [
+    'empty discounts' => [
+        ['discounts' => []], ['discounts' => 'The discounts field is required.'],
+    ],
+    'discount that does not exist' => [
+        ['discounts' => [-1]], ['discounts.0' => 'The selected discounts.0 is invalid.'],
+    ],
+]);
+
+test('it should return unprocessable entity when trying to assign a new discount to a campaign with an invalid payload', function ($payload, $expectedErrors) {
+    $key = array_keys($expectedErrors);
+
+    $campaign = Campaign::factory()->create();
+
+    $response = $this->postJson(route('api.campaigns.assign-discounts', ['campaign' => $campaign->id]), $payload);
+
+    $response->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY);
+
+    $response->assertJsonValidationErrors($key);
+
+    $response->assertJsonFragment([
+        'errors' => [
+            $key[0] => [$expectedErrors[$key[0]]],
+        ],
+    ]);
+
+    if (! empty($payload['discounts'])) {
+        $this->assertDatabaseMissing('campaign_discount_pivot', [
+            'campaign_id' => $campaign->id,
+            'discount_id' => $payload['discounts'][0],
+        ]);
+    }
+
+    $this->assertDatabaseCount('campaign_discount_pivot', 0);
+})->with('invalid_payload');
