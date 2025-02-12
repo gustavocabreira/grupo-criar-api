7a26d23e6abfd665e300d1981c73f9d05e90d1bd
Gustavo Cabreira <gustavo.softdev@gmail.com>
2025-02-12 13:49:56 -0300

    test: it should return not found when trying to remove a discount from a campaign that does not exists

--- tests/Feature/Api/Campaign/Discount/RemoveDiscountFromCampaignTest.php
+++ tests/Feature/Api/Campaign/Discount/RemoveDiscountFromCampaignTest.php
@@ -26,3 +26,24 @@ test('it should be able to remove a discount from a campaign', function () {
 
     $this->assertDatabaseCount('campaign_discount_pivot', 1);
 });
+
+test('it should return not found when trying to remove a discount from a campaign that does not exists', function () {
+    $campaign = Campaign::factory()->create();
+    $discount = Discount::factory()->create();
+
+    $payload = [
+        'discount_id' => $discount->id,
+    ];
+
+    $response = $this->postJson(route('api.campaigns.remove-discounts', ['campaign' => -1]), $payload);
+
+    $response
+        ->assertStatus(Response::HTTP_NOT_FOUND)
+        ->assertJsonPath('message', 'No query results for model [App\Models\Campaign] -1');
+
+    $this->assertDatabaseMissing('campaign_discount_pivot', [
+        'campaign_id' => $campaign->id,
+        'discount_id' => $discount->id,
+        'is_active' => true,
+    ]);
+});
