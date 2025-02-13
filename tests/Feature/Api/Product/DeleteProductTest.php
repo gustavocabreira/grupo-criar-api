b914130f484605dd68c76872a6b9faf55231c723
Gustavo Cabreira <gustavo.softdev@gmail.com>
2025-02-13 12:13:39 -0300

    test: it should return not found when trying to delete a product that does not exists

--- tests/Feature/Api/Product/DeleteProductTest.php
+++ tests/Feature/Api/Product/DeleteProductTest.php
@@ -13,3 +13,17 @@ test('it should be able to delete a product', function () {
 
     $this->assertDatabaseMissing($model->getTable(), $product->toArray());
 });
+
+test('it should return not found when trying to delete a product that does not exists', function () {
+    $model = new Product();
+
+    $product = Product::factory()->create();
+
+    $response = $this->deleteJson(route('api.products.destroy', ['product' => -1]));
+
+    $response
+        ->assertStatus(Response::HTTP_NOT_FOUND)
+        ->assertJsonPath('message', 'No query results for model [App\Models\Product] -1');
+
+    $this->assertDatabaseHas($model->getTable(), $product->toArray());
+});
