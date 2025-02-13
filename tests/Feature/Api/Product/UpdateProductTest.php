4be77111d8328397f77812a8b56a4200c62bd08b
Gustavo Cabreira <gustavo.softdev@gmail.com>
2025-02-13 12:12:09 -0300

    test: it should return not found when trying to update a product that does not exist

--- tests/Feature/Api/Product/UpdateProductTest.php
+++ tests/Feature/Api/Product/UpdateProductTest.php
@@ -14,3 +14,13 @@ test('it should be able to update a product', function () {
 
     $this->assertDatabaseHas($model->getTable(), ['id' => $product->id, ...$payload]);
 });
+
+test('it should return not found when trying to update a product that does not exist', function () {
+    $payload = Product::factory()->make()->toArray();
+
+    $response = $this->putJson(route('api.products.update', ['product' => -1]), $payload);
+
+    $response
+        ->assertStatus(Response::HTTP_NOT_FOUND)
+        ->assertJsonPath('message', 'No query results for model [App\Models\Product] -1');
+});
