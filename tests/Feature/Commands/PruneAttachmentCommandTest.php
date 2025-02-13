fe6c529370893f044fee06935b71708ac0e99909
Gustavo Cabreira <gustavo.softdev@gmail.com>
2025-02-13 13:43:41 -0300

    test: it should not delete attachments that are linked to a product

--- tests/Feature/Commands/PruneAttachmentCommandTest.php
+++ tests/Feature/Commands/PruneAttachmentCommandTest.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\Attachment;
+use App\Models\Product;
 use Carbon\Carbon;
 use Illuminate\Http\UploadedFile;
 use Illuminate\Support\Facades\Storage;
@@ -18,4 +20,24 @@ test('it should be able to prune attachments', function () {
     $this->artisan('attachment:prune');
 
     Storage::disk('public')->assertMissing($response->json()['path']);
+});
+
+test('it should not delete attachments that are linked to a product', function () {
+    Storage::fake('public');
+
+    $image = UploadedFile::fake()->image('example.jpg');
+    $path = $image->store('public', 'public');
+    $attachment = Attachment::factory()->create([
+        'path' => $path,
+    ]);
+    $product = Product::factory()->create();
+    $product->attachments()->attach($attachment->id);
+
+    $this->artisan('attachment:prune');
+
+    Storage::disk('public')->assertExists($path);
+    $this->assertDatabaseHas('product_attachment_pivot', [
+        'attachment_id' => $attachment->id,
+        'product_id' => $product->id,
+    ]);
 });
