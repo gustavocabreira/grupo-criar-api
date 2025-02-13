2e0075b17390b8837d8a3df0d40f2363c1dc54b4
Gustavo Cabreira <gustavo.softdev@gmail.com>
2025-02-13 12:50:04 -0300

    test: it should return unprocessable entity when trying to create an attachment with an invalid payload

--- tests/Feature/Api/Attachment/CreateAttachmentTest.php
+++ tests/Feature/Api/Attachment/CreateAttachmentTest.php
@@ -26,3 +26,36 @@ test('it should be able to create an attachment', function () {
 
     Storage::disk('temp')->assertExists($response->json()['path']);
 });
+
+dataset('invalid_payload', [
+    'empty image' => [
+        ['image' => null], ['image' => ['The image field is required.']],
+    ],
+    'invalid mime type' => [
+        ['image' => UploadedFile::fake()->image('example.pdf')], ['image' => ['The image field must be a file of type: jpeg, png, jpg.', 'The image field must be an image.']],
+    ],
+    'invalid file size' => [
+        ['image' => UploadedFile::fake()->image('example.jpg')->size(1024 * 1024 * 10)],
+        ['image' => ['The image field must not be greater than 1024 kilobytes.']],
+    ],
+]);
+
+test('it should return unprocessable entity when trying to create an attachment with an invalid payload', function ($payload, $expectedErrors) {
+    $key = array_keys($expectedErrors);
+
+    $model = new Attachment();
+
+    $response = $this->postJson(route('api.attachments.store'), $payload);
+
+    $response->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY);
+
+    $response->assertJsonValidationErrors($key);
+
+    $response->assertJsonFragment([
+        'errors' => [
+            'image' => $expectedErrors['image'],
+        ],
+    ]);
+
+    $this->assertDatabaseCount($model->getTable(), 0);
+})->with('invalid_payload');
