2e65c6ce3f6756c7d61c8fb3f2a0d10090961cbe
Gustavo Cabreira <gustavo.softdev@gmail.com>
2025-02-13 16:49:28 -0300

    feat: it should set discounts relation inactive when campaign is inactive

--- app/Observers/CampaignObserver.php
+++ app/Observers/CampaignObserver.php
@@ -10,4 +10,12 @@ class CampaignObserver
     {
         $campaign->is_active = true;
     }
+
+    public function updated(Campaign $campaign): void
+    {
+        if (!$campaign->is_active) {
+            $campaign->activeClusters()->update(['cluster_campaign_pivot.is_active' => false]);
+            $campaign->activeDiscounts()->update(['campaign_discount_pivot.is_active' => false]);
+        }
+    }
 }
--- tests/Feature/Api/Campaign/SetActiveStatusTest.php
+++ tests/Feature/Api/Campaign/SetActiveStatusTest.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Models\Campaign;
+use App\Models\Discount;
 use Illuminate\Http\Response;
 
 test('it should be able to make a campaign active', function () {
@@ -31,4 +32,31 @@ test('it should return not found when trying to set status of a campaign that do
     $response
         ->assertStatus(Response::HTTP_NOT_FOUND)
         ->assertJsonPath('message', 'No query results for model [App\Models\Campaign] -1');
+});
+
+test('it should set discounts relation inactive when campaign is inactive', function () {
+    $model = new Campaign();
+    $campaign = Campaign::factory()->create(['is_active' => true]);
+    $discount = Discount::factory()->create(['is_active' => true]);
+
+    $campaign->discounts()->attach([$discount->id], ['is_active' => true]);
+
+    $payload = [
+        'is_active' => false,
+    ];
+
+    $response = $this->patchJson(route('api.campaigns.set-active-status', ['campaign' => $campaign->id]), $payload);
+
+    $response->assertStatus(Response::HTTP_NO_CONTENT);
+
+    $this->assertDatabaseHas($model->getTable(), [
+        'id' => $campaign->id,
+        ...$payload,
+    ]);
+
+    $this->assertDatabaseHas('campaign_discount_pivot', [
+        'campaign_id' => $campaign->id,
+        'discount_id' => $discount->id,
+        'is_active' => false,
+    ]);
 });
