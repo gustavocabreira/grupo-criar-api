d228c67a4e7bfb4fdfa657e64febcc9ff9439143
Gustavo Cabreira <gustavo.softdev@gmail.com>
2025-02-13 17:02:24 -0300

    docs: endpoint description

--- app/Http/Controllers/Api/ClusterController.php
+++ app/Http/Controllers/Api/ClusterController.php
@@ -16,12 +16,18 @@ use Illuminate\Http\Response;
 
 class ClusterController extends Controller
 {
+    /**
+     * List clusters.
+     */
     public function index(IndexClusterRequest $request, IndexClusterAction $action): JsonResponse
     {
         $cluster = $action->handle($request);
         return response()->json($cluster, Response::HTTP_OK);
     }
 
+    /**
+     * Create a new cluster.
+     */
     public function store(CreateClusterRequest $request): JsonResponse
     {
         $validated = $request->validated();
@@ -31,12 +37,18 @@ class ClusterController extends Controller
         return response()->json($cluster, Response::HTTP_CREATED);
     }
 
+    /**
+     * Show a cluster.
+     */
     public function show(Cluster $cluster, ShowClusterRequest $request, ShowClusterAction $action): JsonResponse
     {
         $cluster = $action->handle($cluster, $request);
         return response()->json($cluster, Response::HTTP_OK);
     }
 
+    /**
+     * Update a cluster.
+     */
     public function update(Cluster $cluster, UpdateClusterRequest $request): JsonResponse
     {
         $validated = $request->validated();
@@ -46,6 +58,9 @@ class ClusterController extends Controller
         return response()->json(null, Response::HTTP_NO_CONTENT);
     }
 
+    /**
+     * Delete a cluster.
+     */
     public function destroy(Cluster $cluster): JsonResponse
     {
         $cluster->delete();
@@ -53,6 +68,9 @@ class ClusterController extends Controller
         return response()->json(null, Response::HTTP_NO_CONTENT);
     }
 
+    /**
+     * Set the active status of a cluster.
+     */
     public function setActiveStatus(Cluster $cluster, SetActiveStatusClusterRequest $request): JsonResponse
     {
         $validated = $request->validated();
