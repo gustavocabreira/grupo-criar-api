90f2f7c1e985fab0e8da1fa700d2b3e544e67065
Gustavo Cabreira <gustavo.softdev@gmail.com>
2025-02-13 15:33:29 -0300

    test: add relationship tests

--- tests/Feature/Api/Discount/ShowDiscountTest.php
+++ tests/Feature/Api/Discount/ShowDiscountTest.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Campaign;
 use App\Models\Discount;
 use Illuminate\Http\Response;
 
@@ -31,4 +32,57 @@ test('it should return not found when trying to find a discount that does not ex
     $response
         ->assertStatus(Response::HTTP_NOT_FOUND)
         ->assertJsonPath('message', 'No query results for model [App\Models\Discount] -1');
+});
+
+test('it should return a discount with its active campaigns', function () {
+    $model = new Discount();
+    $campaignModel = new Campaign();
+
+    $discount = Discount::factory()->create();
+    $campaign = Campaign::factory()->create();
+    $campaign->discounts()->attach([$discount->id], ['is_active' => true]);
+
+    $response = $this->getJson(route('api.discounts.show', [
+        'discount' => $discount->id,
+        'includes' => 'activeCampaigns',
+    ]));
+
+    $response->assertStatus(Response::HTTP_OK)
+        ->assertJsonStructure([
+            ...$model->getFillable(),
+            'active_campaigns' => [
+                '*' => $campaignModel->getFillable(),
+            ],
+        ]);
+
+    expect($response->json()['active_campaigns'][0]['is_active'])->toBeTrue()
+        ->and(count($response->json()['active_campaigns']))->toBe(1);
+});
+
+test('it should return a discount with all its campaigns', function () {
+    $model = new Discount();
+    $campaignModel = new Campaign();
+
+    $discount = Discount::factory()->create();
+    $campaign = Campaign::factory()->create();
+    $anotherCampaign = Campaign::factory()->create();
+    $campaign->discounts()->attach([$discount->id], ['is_active' => true]);
+    $discount->campaigns()->attach([$anotherCampaign->id], ['is_active' => false]);
+
+    $response = $this->getJson(route('api.discounts.show', [
+        'discount' => $discount->id,
+        'includes' => 'campaigns',
+    ]));
+
+    $response->assertStatus(Response::HTTP_OK)
+        ->assertJsonStructure([
+            ...$model->getFillable(),
+            'campaigns' => [
+                '*' => $campaignModel->getFillable(),
+            ],
+        ]);
+
+    expect($response->json()['campaigns'][0]['pivot']['is_active'])->toBe(1)
+        ->and(count($response->json()['campaigns']))->toBe(2)
+        ->and($response->json()['campaigns'][1]['pivot']['is_active'])->toBe(0);
 });
