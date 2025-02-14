e6b0b98e0a60d7d261e70ac899dc944a3f913318
Gustavo Cabreira <gustavo.softdev@gmail.com>
2025-02-14 05:29:30 -0300

    chore: few seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Cluster;
+use App\Models\State;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -15,9 +17,62 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        $states = State::factory()->createMany([
+            [
+                'name' => 'Rio de Janeiro',
+                'acronym' => 'RJ',
+            ],
+            [
+                'name' => 'São Paulo',
+                'acronym' => 'SP',
+            ],
+            [
+                'name' => 'Rio Grande do Sul',
+                'acronym' => 'RS',
+            ],
+            [
+                'name' => 'Paraná',
+                'acronym' => 'PR',
+            ],
         ]);
+
+        $rioDeJaneiroCities = $states[0]->cities()->createMany([
+            ['name' => 'Rio de Janeiro'],
+            ['name' => 'Arraial do Cabo'],
+            ['name' => 'Volta Redonda'],
+        ]);
+
+        $saoPauloCities = $states[1]->cities()->createMany([
+            ['name' => 'São Paulo'],
+            ['name' => 'Guarulhos'],
+            ['name' => 'Campinas'],
+        ]);
+
+        $rioGrandeDoSulCities = $states[2]->cities()->createMany([
+            ['name' => 'Porto Alegre'],
+            ['name' => 'Caxias do Sul'],
+            ['name' => 'Pelotas'],
+        ]);
+
+        $paranaCities = $states[3]->cities()->createMany([
+            ['name' => 'Curitiba'],
+            ['name' => 'Foz do Iguaçu'],
+            ['name' => 'Maringá'],
+        ]);
+
+        $clusters = Cluster::factory()->createMany([
+            [
+                'name' => 'Sudeste',
+            ],
+            [
+                'name' => 'Sul',
+            ]
+        ]);
+
+        $clusters[0]->cities()->attach($rioDeJaneiroCities->pluck('id')->toArray(), ['is_active' => true]);
+        $clusters[0]->cities()->attach($saoPauloCities->pluck('id')->toArray(), ['is_active' => true]);
+
+        $clusters[1]->cities()->attach($rioGrandeDoSulCities->pluck('id')->toArray(), ['is_active' => true]);
+        $clusters[1]->cities()->attach($paranaCities->pluck('id')->toArray(), ['is_active' => true]);
     }
 }
